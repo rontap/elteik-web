32cf986562021ec17f4aae7e5c237b7eaa6f09ac
Aron Tatai <aron.tatai@gmail.com>
2021-12-01 14:15:12 +0100

    game changes

--- bead1/js/main.js
+++ bead1/js/main.js
@@ -1,6 +1,4 @@
 function beginGame() {
-
-
   window.ctx = canv.getContext('2d');
   let dim = Canvas.unit * 8.5;
   canv.width = dim;
@@ -17,12 +15,11 @@ function beginGame() {
 //const thisGame = new Game({});
 
   let rawPlayers = [new Player(), new Player(), new Player(), new Player()]
-  window.theGame = new Game( rawPlayers.slice(0,players), Number(notrs.value));
+  window.theGame = new Game(rawPlayers.slice(0, players), Number(notrs.value));
   window.renderer = new Renderer(ctx, theGame);
   window.rendererOdd = new Renderer(ctx2, theGame, true);
-//const a1 = [n1, s1, t1, t2];
+
   theGame.render();
-//a1.map(a => r.render(a) )
   odd.classList.remove('no');
   control.classList.remove('no');
   canv.classList.remove('no');
@@ -33,31 +30,36 @@ function beginGame() {
 //beginGame();
 
 let players;
+
 function setPlayers(event) {
   players = Number(event.target.value);
   notrs.innerHTML = "";
-  for (let i = 1 ; i < 24/players ; i++) {
-    notrs.innerHTML+=`<option ${i===2?'selected':''} value='${i}'>${i} per player</option>`
+  for (let i = 1; i < 24 / players; i++) {
+    notrs.innerHTML += `<option ${i === 2 ? 'selected' : ''} value='${i}'>${i} per player</option>`
   }
 }
-setPlayers({target:{value:2}});
+
+setPlayers({target: {value: 2}});
 
 function load() {
   if (localStorage.gs) {
     beginGame();
-    theGame.fromJSON( JSON.parse(localStorage.gs) )
+    theGame.fromJSON(JSON.parse(localStorage.gs))
   } else {
     alert('no saves available!');
   }
 }
+
 function save() {
   if (theGame.save())
-  alert('saved!')
+    alert('saved!')
 }
+
 function showHelp() {
   descriptionRules.classList.remove('no');
   mainScreen.classList.add('no');
 }
+
 function hideHelp() {
   mainScreen.classList.remove('no');
   descriptionRules.classList.add('no');
